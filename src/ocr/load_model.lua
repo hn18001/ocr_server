b280a3de96fe7ba259be52fd4eb1a6a107086c53
hn18001 <hn18001@gmail.com>
2016-11-15 16:30:54 +0800

    Add new files to lua ocr.

---
+++ src/ocr/load_model.lua
@@ -0,0 +1,85 @@
+start = os.clock()
+require('nn')
+
+require('cutorch')
+require('cunn')
+require('cudnn')
+require('optim')
+require('paths')
+require('nngraph')
+
+-- Set the modules' path and the library's path
+package.path = package.path .. ";/data/heneng/work/QYDnnOCR/trunk/src/?.lua;/data/heneng/work/QYDnnOCR/trunk/third_party/lmdb-lua-ffi/src/?.lua;"
+package.cpath = package.cpath .. ";/data/heneng/work/QYDnnOCR/trunk/src/?.so"
+
+require('libcrnn')
+require('utilities')
+require('inference')
+require('CtcCriterion')
+require('DatasetLmdb')
+require('LstmLayer')
+require('BiRnnJoin')
+require('SharedParallelTable')
+
+require('lfs')
+
+print("Load all models: ".. (os.clock()-start))
+
+function make_dict(duri, id2char_tb)
+    fp = io.open(duri, 'r')
+    assert(fp)
+    cc = 0
+    for line in fp:lines() do
+        --print(line)
+        cc = cc + 1
+        id2char_tb[cc] = line
+    end
+    fp:close()
+end
+
+start = os.clock()
+arg = {}
+arg[1] = 3
+arg[2] = "snapshot_138000.t7"
+local dev_id = tonumber(arg[1])
+local wordlisturi = '/data/heneng/work/QYDnnOCR/trunk/src/word_3567.txt'
+id2char_tb = {}
+make_dict(wordlisturi, id2char_tb)
+
+local img_tb ={}
+
+cutorch.setDevice(dev_id)
+torch.setnumthreads(4)
+torch.setdefaulttensortype('torch.FloatTensor')
+
+print("make_dict(): "..(os.clock() - start))
+start = os.clock()
+--print('Loading model...')
+-- Test 1
+--local modelDir = '/data/zhangyg/work_torch/model/crnn/3567_5m_4color_7font_shadow_480'
+--paths.dofile(paths.concat(modelDir, 'config.lua'))
+--local modelLoadPath = paths.concat(modelDir, 'snapshot_168000.t7')
+local modelDir = '/data/heneng/work/QYDnnOCR/trunk/model/'
+paths.dofile(paths.concat(modelDir, 'config.lua'))
+--local modelLoadPath = paths.concat(modelDir, 'snapshot_174000.t7')i
+local modelName = arg[2]
+local modelLoadPath = paths.concat(modelDir,modelName) 
+print("dofile config.lua: ".. (os.clock() - start))
+start = os.clock()
+
+gConfig = getConfig()
+gConfig.modelDir = modelDir
+gConfig.maxT = 0
+model, criterion = createModel(gConfig)
+--print(modelLoadPath)
+local snapshot = torch.load(modelLoadPath)
+--print ('snapshot end')
+
+print("createModel: ".. (os.clock() - start))
+start = os.clock()
+loadModelState(model, snapshot)
+model:evaluate()
+--print(string.format('Model loaded from %s', modelLoadPath))
+
+print("model:evaluate(): ".. (os.clock() - start))
+print("Load model finished.")
---
+++ src/ocr/ocr.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+import ctypes
+lualib = ctypes.CDLL('libluajit.so', mode=ctypes.RTLD_GLOBAL)
+import lupa
+from lupa import LuaRuntime
+lua = LuaRuntime(unpack_returned_tuples=True)
+
+def ocr(img_path):
+    # Attention: the path of load_model and recognize!!!
+    lua.eval('require ("./ocr/load_model")')
+    lua_ocr = lua.eval('''
+        function(img_path)
+            require('./ocr/recognize')
+            result = recognize(img_path)
+
+            return result
+        end
+    ''')
+    result = lua_ocr(img_path)
+    result = result.encode('utf-8')
+    return result
+
+if __name__ == "__main__":
+    file_name = "../../result/0.jpg"
+    print ocr(file_name)
