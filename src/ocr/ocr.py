c4539a7b1faac0f7890ee1a75dad62b5e72ca7b4
heneng <heneng@qiyi.com>
2016-11-15 11:55:44 +0800

    Add main func.

--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -7,11 +7,13 @@ import lupa
 from lupa import LuaRuntime
 lua = LuaRuntime(unpack_returned_tuples=True)
 
+
+
 def load_model():
     lua.eval('require ("load_model")')
 
 def ocr(img_path):
-    #lua.eval('require ("load_model")')
+    lua.eval('require ("load_model")')
     lua_ocr = lua.eval('''
         function(img_path)
             require('recognize')
@@ -24,5 +26,7 @@ def ocr(img_path):
     result = result.encode('utf-8')
     return result
 
-#load_model()
-#print ocr("../result/0.jpg")
+
+if __name__ == "__main__":
+    load_model()
+    print ocr("../result/0.jpg")
