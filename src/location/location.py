6f171fd109d1e2267276895b62c9ff048b027bd1
何能（He Neng） <heneng@iqiyi.pps>
2016-11-16 13:10:15 +0800

    Add location code.

---
+++ src/location/location.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+import traceback,sys
+import time
+#import cv2
+import os
+import numpy as np
+import thriftpy
+from thriftpy.transport import TFramedTransportFactory
+from thriftpy.rpc import make_client
+CNN_i2i_thrift = thriftpy.load('CNN_image2image.thrift', module_name="CNN_image2image_thrift")
+import multiprocessing
+from multiprocessing import Pool
+from PIL import Image
+
+class CNNClient:
+    def __init__(self, ip="10.15.208.61", port=6800):
+        self.ip = ip
+        self.port = port
+        try:
+            self.client = make_client(CNN_i2i_thrift.CNNPredictService,
+                    ip, port,trans_factory=TFramedTransportFactory(), timeout=12000) ## timeout ms
+            print ip+':'+str(port)
+
+        except Exception as  err:
+            print err
+            traceback.print_exc()
+
+        return
+
+    def DoPostProcess(self, datalst):
+        reqlst = []
+        for data in datalst:
+            req = CNN_i2i_thrift.Request()
+            req.inputImage = data
+            reqlst.append(req)
+
+        ret = []
+        try:
+            print '----'
+            reslst = self.client.predict(reqlst)
+            print '----', len(reslst)
+            #print res
+            for i in range(len(reslst)):
+                roiimglst = []
+                ## get mask image
+                maskres = reslst[i].maskResult
+                print type(maskres.outputImage), len(maskres.outputImage)
+                print maskres.imageChannel
+                print maskres.imageWidth
+                print maskres.imageHeight
+                roiimglst.append(maskres.outputImage)
+
+                ## get roi image
+                roilist = reslst[i].roiResults
+                for k in range(len(roilist)):
+                    roi = roilist[k]
+                    val = roi.outputImage
+                    resc= roi.imageChannel
+                    resw= roi.imageWidth
+                    resh= roi.imageHeight
+                    print 'c,h,w',resc,resh,resw
+                    print 'size:',len(val)
+
+                    roiimglst.append(val)
+                ret.append(roiimglst)
+
+        except Exception as err:
+            try:
+                self.client = make_client(CNN_i2i_thrift.CNNPredictService,
+                        self.ip, self.port,trans_factory=TFramedTransportFactory(), timeout=12000) ## timeout ms
+            except Exception as  err:
+                traceback.print_exc()
+                print err
+            print err
+            traceback.print_exc()
+        return ret
+
+def create_opencv_image_from_stream(stream):
+    img_array = np.asarray(bytearray(stream), dtype=np.uint8)
+    return cv2.imdecode(img_array, cv2.IMREAD_UNCHANGED)
+
+def create_pil_image_from_buffer(buf):
+    import cStringIO
+    im = Image.open(cStringIO.stringIO(buf))
+    return im
+
+def save_scene_boxes(file_stream_list, index, scene_boxes):
+    src_img = create_pil_image_from_buffer(file_stream_list[index])
+    print "image size:", src_img.size
+
+    results = []
+
+    border = 0
+    for i, box in enumerate(scene_boxes):
+        left = box['left']
+        top = box['top'] + border
+        width = box['width']
+        height = box['height'] - border
+
+        left = left / 500.0 * src_img.size[0]
+        top = top / 500.0 * src_img.size[1]
+        width = width / 500.0 * src_img.size[0]
+        height = height / 500.0 * src_img.size[1]
+        crop_img = src_img.crop((left, top, left + width, top + height))
+
+        pid = os.getpid()
+        path_prefix = "./result/" + str(pid)
+        if not os.path.exists(path):
+            os.command("mkdir " + path_prefix)
+
+        save_name = path_prefix + "/" + str(index) + "_" + str(i) + ".jpg"
+        print save_name
+        crop_img.save(save_name)
+
+        results.append(save_name)
+
+    return results
+
+def scene_location(file_list):
+    print "Entering scene_location()..."
+    hd = CNNClient(ip="10.15.208.61", port=6800)
+
+    print len(file_list)
+    retlist = hd.DoPostProcess(file_list)
+
+    results = []
+    for j in range(len(retlist)):
+        ret = retlist[j]
+        for i in range(len(ret)):
+            roi = ret[i]
+            save_dir = ""
+            save_all_path = ""
+            if i == 0:
+                import util
+                img = create_opencv_image_from_stream(roi)
+                scene_boxes = util.get_scene_box(img)
+                print "scene boxes' len:", len(scene_boxes)
+                files = save_scene_boxes(file_list, j, scene_boxes)
+                results.append(files)
+            
+    return results
+
+def crop_img(file_name):
+    from PIL import Image
+    im = Image.open(file_name)
+    width, height = im.size
+
+    crop_im = im.crop((0, 2*height/3, width, height))
+    basename = os.path.basename(file_name)
+    tempfile = "./temp/" + basename
+    crop_im.save(tempfile)
+    return tempfile
---
+++ src/location/util.py
@@ -0,0 +1,97 @@
+import cv2
+import os
+
+def get_bounding_boxes(src_img):
+    gray_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
+    #gray_img = 255 - gray_img
+
+    connectivity = 4
+
+    output = cv2.connectedComponentsWithStats(gray_img, connectivity, cv2.CV_32S)
+
+    num_labels = output[0]
+
+    labels = output[1]
+
+    stats = output[2]
+    centroids = output[3]
+
+    b_boxes = []
+    for i in range(1, num_labels): # The first rect is the whole image, it's not needed
+        result = {}
+        left = stats[i][0]
+        top = stats[i][1]
+        width = stats[i][2]
+        height = stats[i][3]
+        area = stats[i][4]
+
+        right = left + width
+        bottom = top + height
+
+        #rect_img = cv2.rectangle(src_img, (left, top), (right, bottom), (0, 0, 255), 3)
+
+        result["left"] = left
+        result["top"]  = top
+        result['width'] = width
+        result['height'] = height
+        result['area'] = area
+
+        b_boxes.append(result)
+
+    #cv2.imwrite("./rect.jpg", rect_img)
+
+    return b_boxes
+
+def remove_area(img_width, img_height, b_boxes):
+    area_thresh = 1000
+    width_thresh = 20
+
+    possible_scenes = []
+    print b_boxes
+    for b_box in b_boxes:
+        left = b_box['left']
+        width = b_box['width']
+        area = b_box['area']
+
+        width_middle = left + width / 2
+        width_gap = img_width / 2 - width_middle
+
+        print width_gap
+        if abs(width_gap) < width_thresh and area > area_thresh:
+            possible_scenes.append(b_box)
+
+    return possible_scenes
+
+def get_scene_box(src_img):
+    #src_img = cv2.imread(img_name, cv2.IMREAD_COLOR)
+
+    b_boxes = get_bounding_boxes(src_img)
+
+    img_width = src_img.shape[0]
+    img_height = src_img.shape[1]
+    possible_scenes = remove_area(img_width, img_height, b_boxes)
+
+    print "possible scenes:", len(possible_scenes)
+    for box in possible_scenes:
+        left = box['left']
+        top = box['top']
+        width = box['width']
+        height = box['height']
+
+        right = left + width
+        bottom = top + height
+
+        src_img = cv2.rectangle(src_img, (left, top), (right, bottom), (0, 0, 255), 3)
+
+    cv2.imwrite("./scene_img.jpg", src_img)
+
+    return possible_scenes
+
+if __name__ == "__main__":
+    img_name = "2a9c418035365058470958302f7649e6_269_0.jpg"
+    img_name = "./mask/" + img_name
+
+    src_img = cv2.imread(img_name, cv2.IMREAD_COLOR)
+    get_scene_box(src_img)
+
+
