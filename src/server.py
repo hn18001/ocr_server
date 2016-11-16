090e09c07b0c88ad4bed36466a0df89364c7091e
heneng <heneng@qiyi.com>
2016-11-16 16:03:22 +0800

    In scene_ocr(), decide wether apply scene loation or not, according to the value of b_location,

--- src/server.py
+++ src/server.py
@@ -41,25 +41,38 @@ class Handler:
         print 'fuct1()'
 
     def scene_ocr(self, images, b_location):
-        ocr_results = []
-        for index, image in enumerate(images):
-            save_name = "../result/"+ str(index) + ".jpg"
-            f=open(save_name, 'w')
-            f.write(image)
+        rlt_results = []
+        for image in images:
+            ocr_results = ""
+            if b_location == True:
+                file_list = []
+                file_list.append(image)
+                # The return of scene_location is a list of a scene image list,
+                # it means that a source image may have many scene images.
+                loc_results = location.scene_location(file_list)
 
-            result = ocr.ocr(save_name)
-            #print ocr_result
-            ocr_results.append(result)
+                print "len of loc_results:", len(loc_results)
+                print loc_results
+                for loc_result in loc_results:
+                    for scene_result in loc_result:
+                        result = ocr.ocr(scene_result)
+                        ocr_results = ocr_results + result + ";"
+            else:
+                pid = os.getpid()
+                file_prefix = "../result/" + str(pid)
+                if not os.path.exists(file_prefix):
+                    os.system("mkdir " + file_prefix)
 
-        results = []
-        for result in ocr_results:
-            rlt = ocr_result(result=result, roi_left=10, roi_top=10, roi_width=100, roi_height=100)
-            results.append(rlt)
+                save_name = file_prefix + "/src_img.jpg"
+                with open(save_name, 'w') as f:
+                    f.write(image)
 
-        #result = ocr_result(result="234", roi_left=10, roi_top=10, roi_width=100, roi_height=100)
-        #results.append(result)
-        #print "results:", results
-        return results
+                ocr_results = ocr.ocr(save_name)
+
+            rlt = ocr_result(result=ocr_results, roi_left=0, roi_top=0, roi_width=0, roi_height=0)
+            rlt_results.append(rlt)
+
+        return rlt_results
 
 def main():
     handler = Handler()
@@ -72,10 +85,10 @@ def main():
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
 
-    #server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
-    from thrift.server import TProcessPoolServer
-    server = TProcessPoolServer.TProcessPoolServer(processor, transport, tfactory, pfactory) # When there is no call, the sub-thread will not be awaken.
-    server.setNumWorkers(3) # Set the number of processes.
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    #from thrift.server import TProcessPoolServer
+    #server = TProcessPoolServer.TProcessPoolServer(processor, transport, tfactory, pfactory) # When there is no call, the sub-thread will not be awaken.
+    #server.setNumWorkers(3) # Set the number of processes.
     #server = TServer.TForkingServer(processor, transport, tfactory, pfactory)
 
     print "Starting the server"
@@ -86,20 +99,21 @@ def test_location():
 
     file_list = []
     for root, dir_names, file_names in os.walk(path):
-        for file_name in file_names:
+        for index, file_name in enumerates(file_names):
+            file_list = []
             full_name = os.path.join(root, file_name)
             print full_name
             with open(full_name, 'rb') as f:
                 buf = f.read()
                 file_list.append(buf)
 
-    print "len of file_list: ", len(file_list)
-    results = location.scene_location(file_list)
+            print "len of file_list: ", len(file_list)
+            results = location.scene_location(file_list)
 
-    for result in results:
-        for file in result:
-            print file
+            for result in results:
+                for file in result:
+                     print file
 
 if __name__ == "__main__":
-    #main()
-    test_location()
+    main()
+    #test_location()
